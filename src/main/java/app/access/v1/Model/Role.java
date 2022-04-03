5d0e0fc0820e0ec1601a84a7c8b140059d018247
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-03 17:17:03 +0300

    Start Building the Access System

---
+++ src/main/java/app/access/v1/Model/Role.java
@@ -0,0 +1,22 @@
+package app.access.v1.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 60)
+    private String name;
+}
