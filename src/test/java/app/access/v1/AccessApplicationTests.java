71e31439c34badd9624684c888da15322c953e60
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-03 17:10:56 +0300

    Start Building the Access System

---
+++ src/test/java/app/access/v1/AccessApplicationTests.java
@@ -0,0 +1,13 @@
+package app.access.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
