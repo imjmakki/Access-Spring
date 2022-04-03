f830b7d2ef234e88bd99e32c4408716303fce789
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-03 17:22:14 +0300

    Start Building the Access System

--- src/main/java/app/access/v1/DAO/RoleRepository.java
+++ src/main/java/app/access/v1/DAO/RoleRepository.java
@@ -1,4 +1,12 @@
 package app.access.v1.DAO;
 
-public interface RoleRepository {
+import app.access.v1.Model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
 }
--- src/main/java/app/access/v1/DAO/UserRepository.java
+++ src/main/java/app/access/v1/DAO/UserRepository.java
@@ -2,9 +2,11 @@ package app.access.v1.DAO;
 
 import app.access.v1.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByUsernameOrEmail(String username, String email);
